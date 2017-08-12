33d05dd06e7b3569a19efacec1178f2d3e065d57
zhc1124 <qwerty762@163.com>
2017-08-13 03:23:02 +0800

    fix

--- net.py
+++ net.py
@@ -1,6 +1,6 @@
 import cv2
 gray = cv2.imread('gray.jpg', 0)
-circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 100, param1=100, param2=10, minRadius=0, maxRadius=10)
+circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 100, param1=100, param2=4, minRadius=0, maxRadius=10)
 print circles
 # cv2.imshow("img", gray)
 # cv2.waitKey(0)
--- video.py
+++ video.py
@@ -3,9 +3,12 @@
 import cv2
 import numpy as np
 # 去掉废的10张
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 height = 480
 width = 640
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
+
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
 
 for i in xrange(10):
     ret, frame = cap.read()
